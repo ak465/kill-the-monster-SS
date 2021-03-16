b4822e3ce3677ce6fe6bd9733915c4e3f3597972
ak465 <76836592+ak465@users.noreply.github.com>
2021-03-16 21:00:54 +0530

    Add files via upload
    files of kill the monster

---
+++ Superhero.js
@@ -0,0 +1,24 @@
+class superhero {
+    constructor(x, y, width, height){
+        var options = {
+            isStatic:false,
+            restitution:0,
+            friction:1,
+            density:1.2
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.height = height;
+        this.width = width;
+        //this.image =loadImage("images/Superhero-01.png");
+        World.add(world, this.body);
+    }
+    display(){
+        var angle = this.body.angle;
+        var pos = this.body.position;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        imageMode(CENTER);
+        pop();
+    }
+
+}
